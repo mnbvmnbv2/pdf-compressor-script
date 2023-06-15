85e65832c7dc2d931e7fd5bda8c38ca19ee7e084
mnbvmnbv2 <ferdinand@ytteborg.com>
2023-06-15 18:24:27 +0200

    add skipping on timeout, unicodeerror and logfile for continuing

--- pdf_compress.py
+++ pdf_compress.py
@@ -19,28 +19,48 @@ def compress_pdf(input_file, output_file):
         input_file,
     ]
 
-    result = subprocess.run(gs_command, stderr=subprocess.PIPE)
+    try:
+        result = subprocess.run(gs_command, stderr=subprocess.PIPE, timeout=300)
+    except subprocess.TimeoutExpired:
+        print(f"Compression timed out for: {input_file}")
+        return False
+    except UnicodeEncodeError:
+        print(f"UnicodeEncodeError occurred while processing: {input_file}")
+        return False
 
     if result.returncode != 0:
-        error_message = result.stderr.decode("utf-8")
-        print(f"An error occurred: {error_message}")
+        try:
+            error_message = result.stderr.decode("utf-8")
+            print(f"An error occurred: {error_message}")
+        except UnicodeEncodeError:
+            print(
+                f"UnicodeEncodeError occurred while decoding error message for: {input_file}"
+            )
         return False
 
     return True
 
 
-def compress_pdfs_in_folder(folder):
+def compress_pdfs_in_folder(folder, log_file):
+    with open(log_file, "a+", encoding="utf-8") as f:
+        f.seek(0)
+        processed_files = f.read().splitlines()
+
     for root, dirs, files in os.walk(folder):
         for file in files:
             if file.endswith(".pdf"):
                 original_file = os.path.join(root, file)
+
+                if original_file in processed_files:
+                    print(f"Skipping {original_file} as it has already been processed.")
+                    continue
+
                 temp_file = os.path.join(root, "temp_compressed.pdf")
 
                 if compress_pdf(original_file, temp_file):
                     original_size = os.path.getsize(original_file)
                     compressed_size = os.path.getsize(temp_file)
 
-                    # Only replace the original file if the compressed file is smaller
                     if compressed_size < original_size:
                         shutil.move(temp_file, original_file)
                         compression_ratio = (
@@ -53,6 +73,9 @@ def compress_pdfs_in_folder(folder):
                             f"Compression of {original_file} would increase file size. Skipping."
                         )
 
+                with open(log_file, "a", encoding="utf-8") as f:
+                    f.write(original_file + "\n")
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(
@@ -61,7 +84,10 @@ if __name__ == "__main__":
     parser.add_argument(
         "folder", help="The path to the folder containing the PDFs to compress."
     )
+    parser.add_argument(
+        "log_file", help="The path to the log file to keep track of processed PDFs."
+    )
 
     args = parser.parse_args()
 
-    compress_pdfs_in_folder(args.folder)
+    compress_pdfs_in_folder(args.folder, args.log_file)
